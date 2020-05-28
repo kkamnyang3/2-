94dd0e08174f0ba7676d78fbb447b04cf6180040
kkamnyang3 <65141862+kkamnyang3@users.noreply.github.com>
2020-05-28 19:53:42 +0900

    파이썬 API 호출연습

--- week3_requests.py
+++ week3_requests.py
@@ -1,4 +1,16 @@
 import requests
 
-reponse = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-print(reponse.json())
+response = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+print(response.json())
+
+# ajax와 달리 response 변수에 바로 딕셔너리가 저장되어 있지 않음
+# ajax처럼 사용하려면 response.json() 함수를 실행시켜야 얻을 수 있음
+result = response.json()
+print(result)
+
+data = result['RealtimeCityAir']['row'] # js랑 같음
+
+for datum in data:
+    print(datum['MSRSTE_NM'], datum['IDEX_MVL'])
+    
+
