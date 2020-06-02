f1e0e8dafd4e3799c0c4451c9418d460ea3403d6
kkamnyang3 <65141862+kkamnyang3@users.noreply.github.com>
2020-06-02 20:02:21 +0900

    Week_코로나 크롤링 연습

---
+++ Week4_corona.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+#크롤링 하고 싶은 사이트 URL
+target_url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(target_url, headers=headers) #'나는 컴퓨터가 아니라 사람이야' / 위장하는거임
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+
+#여기서부터 크롤링 작업
+
+
+#content > div > div:nth-child(5) > table > tbody > tr > td:nth-child(1)
+
+coronas = soup.select('#content > div > div:nth-child(5) > table > tbody > tr > td')
+
+for corona in coronas:
+    print(corona.text)
+
+
+
+# 해답
+
+#content > div > div:nth-child(5) > table > tbody > tr > td 
+
+tds = soup.select('#content > div > div:nth-child(5) > table > tbody > tr > td')
+for td in tds:
+    print(td.text)
