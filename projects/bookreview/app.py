4772170fd1b0dbd5c445d43df6e22bf87acd9586
kkamnyang3 <65141862+kkamnyang3@users.noreply.github.com>
2020-06-04 21:29:12 +0900

    모두의 북리뷰 리뷰 리스트 만들기

--- projects/bookreview/app.py
+++ projects/bookreview/app.py
@@ -34,7 +34,15 @@ def write_review():
 
 @app.route('/reviews', methods=['GET'])
 def read_reviews():
-    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+    reviews = list( # mongodb의 결과들(도큐먼트들)을 리스트로 변환
+        # {} - 검색 조건(비어있으니 모두 다 가져옴)
+        # {'_id':0} - id 정보는 필요없으니 가져오지 않겠다
+        db.reviews.find({},{'_id':0})
+    )
+
+    #[{'title':'테스트 책','author':'테스트 저자','review':'테스트 책'}]
+
+    return jsonify({'result':'success', 'reviews': reviews})
 
 
 if __name__ == '__main__':
