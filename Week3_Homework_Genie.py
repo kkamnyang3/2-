d4c516b818e872677ee143fa1b6bd0cf843f234c
kkamnyang3 <65141862+kkamnyang3@users.noreply.github.com>
2020-06-08 23:19:49 +0900

    Genie 숙제

---
+++ Week3_Homework_Genie.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1')
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+
+#############################
+# (입맛에 맞게 코딩)
+#############################
+
+#아로하, 시작 : title
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(2) > td.info > a.title.ellipsis
+
+# 조정석 : singer
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+
+# rank
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(2) > td.number
+
+musics = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for music in musics:
+    title = music.select_one('td.info > a.title.ellipsis')
+    singer = music.select_one('td.info > a.artist.ellipsis')
+    rank = music.select_one('td.number')
+
+    
+
